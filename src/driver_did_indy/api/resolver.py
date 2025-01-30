79e608eada6ac242f1e34f4defc8bcbf2d855bcc
Daniel Bluhm <dbluhm@pm.me>
2025-01-30 09:24:03 -0600

    feat: resolver endpoint
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

---
+++ src/driver_did_indy/api/resolver.py
@@ -0,0 +1,23 @@
+"""Resolver API."""
+
+from fastapi import APIRouter, HTTPException
+
+from driver_did_indy.depends import LedgersDep
+from driver_did_indy.ledgers import ReadOnlyLedger
+from driver_did_indy.utils import parse_did_indy
+
+router = APIRouter(tags=["Resolver"])
+
+
+@router.get("/resolve/{did}")
+async def get_resolve_did(did: str, ledgers: LedgersDep):
+    """Resolve a did:indy DID."""
+    parsed = parse_did_indy(did)
+
+    pool = ledgers.get(parsed.namespace)
+    if not pool:
+        raise HTTPException(404, detail=f"Namespace {parsed.namespace} is unknown")
+
+    ledger = ReadOnlyLedger(pool)
+    result = await ledger.resolve(did)
+    return result
