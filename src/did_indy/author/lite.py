dd461ef82a0b5fe4810bfdbc1030f6e09370f813
Daniel Bluhm <dbluhm@pm.me>
2025-07-23 21:18:54 -0500

    fix: issuer id is did not url
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- src/did_indy/author/lite.py
+++ src/did_indy/author/lite.py
@@ -14,7 +14,7 @@ from did_indy.anoncreds import (
 )
 from did_indy.author.base import BaseAuthor
 from did_indy.client.client import IndyDriverClient
-from did_indy.did import parse_namespace_from_did_url
+from did_indy.did import parse_namespace_from_did
 from did_indy.driver.api.txns import (
     CredDefSubmitResponse,
     NymResponse,
@@ -89,7 +89,7 @@ class AuthorLite(BaseAuthor):
     ) -> SchemaSubmitResponse:
         """Register a schema."""
         schema = normalize_schema_representation(schema)
-        namespace = parse_namespace_from_did_url(schema.issuer_id)
+        namespace = parse_namespace_from_did(schema.issuer_id)
         taa = taa or await self.dependencies.get_taa(namespace)
         txn = await self.client.create_schema(schema.model_dump(), taa)
         signer = await self.dependencies.get_signer(schema.issuer_id)
@@ -104,7 +104,7 @@ class AuthorLite(BaseAuthor):
     ) -> CredDefSubmitResponse:
         """Register a credential definition."""
         cred_def = normalize_cred_def_representation(cred_def)
-        namespace = parse_namespace_from_did_url(cred_def.issuer_id)
+        namespace = parse_namespace_from_did(cred_def.issuer_id)
         taa = taa or await self.dependencies.get_taa(namespace)
         txn = await self.client.create_cred_def(cred_def.model_dump(), taa)
         signer = await self.dependencies.get_signer(cred_def.issuer_id)
@@ -119,7 +119,7 @@ class AuthorLite(BaseAuthor):
     ) -> RevRegDefSubmitResponse:
         """Register a revocation registry definition."""
         rev_reg_def = normalize_rev_reg_def_representation(rev_reg_def)
-        namespace = parse_namespace_from_did_url(rev_reg_def.issuer_id)
+        namespace = parse_namespace_from_did(rev_reg_def.issuer_id)
         taa = taa or await self.dependencies.get_taa(namespace)
         txn = await self.client.create_rev_reg_def(rev_reg_def.model_dump(), taa)
         signer = await self.dependencies.get_signer(rev_reg_def.issuer_id)
@@ -136,7 +136,7 @@ class AuthorLite(BaseAuthor):
     ) -> RevStatusListSubmitResponse:
         """Register a revocation status list."""
         rev_status_list = normalize_rev_status_list_representation(rev_status_list)
-        namespace = parse_namespace_from_did_url(rev_status_list.issuer_id)
+        namespace = parse_namespace_from_did(rev_status_list.issuer_id)
         taa = taa or await self.dependencies.get_taa(namespace)
         txn = await self.client.create_rev_status_list(
             rev_status_list.model_dump(), taa
@@ -158,7 +158,7 @@ class AuthorLite(BaseAuthor):
         """Update a revocation status list."""
         prev_list = normalize_rev_status_list_representation(prev_list)
         curr_list = normalize_rev_status_list_representation(curr_list)
-        namespace = parse_namespace_from_did_url(curr_list.issuer_id)
+        namespace = parse_namespace_from_did(curr_list.issuer_id)
         taa = taa or await self.dependencies.get_taa(namespace)
         txn = await self.client.update_rev_status_list(
             prev_list.current_accumulator, curr_list.model_dump(), revoked, taa
